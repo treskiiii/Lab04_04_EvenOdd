dd53b1c9528a435bed386c5e2d3f76a5fa64a11f
treskiiii <apontetq@mail.uc.edu>
2025-02-09 16:21:30 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 8; // Hardcoded value to check
+
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        } else {
+            System.out.println(numToExamine + " is Odd.");
+        }
+    }
+}
